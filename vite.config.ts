b56a387fe95c8b1b853f398543735bd1a8469b9f
Alfarizy-w <wahyualfarizy805@gmail.com>
2025-08-24 23:18:59 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// supaya __dirname bisa dipakai di ESM/TypeScript
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -9,8 +14,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  root: ".", // root project = folder repo ini
   build: {
-    outDir: "dist",
+    outDir: "dist", // hasil build ke folder dist
     emptyOutDir: true,
   },
 });
